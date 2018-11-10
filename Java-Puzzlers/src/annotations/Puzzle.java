5f6314a3f9e8269dcf87dac0f94acf375315bb93
Deepak Chaudhary <deepak.kr@live.com>
2018-11-10 17:28:08 +0530

    adding puzzle annotation

---
+++ Java-Puzzlers/src/annotations/Puzzle.java
@@ -0,0 +1,19 @@
+package annotations;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+
+@Documented
+@Retention(SOURCE)
+/**
+ * 
+ */
+
+/**
+ * @author deepak
+ *
+ */
+public @interface Puzzle {
+
+}
